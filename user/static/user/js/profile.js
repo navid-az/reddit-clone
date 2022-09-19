ada67aec6b424df0390b23ab942b84ddc3214778
Navid Azimi <navidazimi1380@gmail.com>
2022-09-19 23:07:39 +0430

    improved user profile comments

--- user/static/user/js/profile.js
+++ user/static/user/js/profile.js
@@ -2,25 +2,35 @@ let userPostsWrapper = document.getElementById("user-posts-wrapper");
 let userCommentsWrapper = document.getElementById("user-comments-wrapper");
 
 const changeView = (btn) => {
-  if (btn == 0) {
-    userCommentsWrapper.style.opacity = "0";
-    setTimeout(() => {
-      userCommentsWrapper.classList.add("d-none");
-      userPostsWrapper.classList.remove("d-none");
-    }, 200);
-    setTimeout(() => {
-      userPostsWrapper.style.opacity = "1";
-    }, 500);
+  if (window.location.href == "http://127.0.0.1:8000/u/reddit/profile/") {
+    if (btn == 0) {
+      userCommentsWrapper.style.opacity = "0";
+      setTimeout(() => {
+        userCommentsWrapper.classList.add("d-none");
+        userPostsWrapper.classList.remove("d-none");
+      }, 200);
+      setTimeout(() => {
+        userPostsWrapper.style.opacity = "1";
+      }, 500);
+    } else {
+      userPostsWrapper.style.opacity = "0";
+      setTimeout(() => {
+        userPostsWrapper.classList.add("d-none");
+        userCommentsWrapper.classList.remove("d-none");
+      }, 200);
+      setTimeout(() => {
+        userCommentsWrapper.style.opacity = "1";
+      }, 500);
+    }
   } else {
-    userPostsWrapper.style.opacity = "0";
-    setTimeout(() => {
-      userPostsWrapper.classList.add("d-none");
-      userCommentsWrapper.classList.remove("d-none");
-    }, 200);
-    setTimeout(() => {
-      userCommentsWrapper.style.opacity = "1";
-    }, 500);
+    window.location.replace("http://127.0.0.1:8000/u/reddit/profile/");
+
+    // this will send btn value to the profile page
+    localStorage.setItem("BTN", btn);
   }
 };
 
-changeView(0);
+if (window.location.href == "http://127.0.0.1:8000/u/reddit/profile/") {
+  var buttonCode = localStorage.getItem("BTN");
+  changeView(buttonCode);
+}
